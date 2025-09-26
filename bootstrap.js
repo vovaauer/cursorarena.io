abf665fc8d9114378640b298298f400886d95635
Vova Auer <mail@vovaauer.com>
2025-09-26 00:29:43 -0600

    Client: Use secure WSS protocol

--- bootstrap.js
+++ bootstrap.js
@@ -2,7 +2,7 @@ import init, { Game } from './pkg/cursorarena_io.js';
 
 // Multiplayer client (dumb renderer)
 async function initMultiplayerGame(mainContent, mapData) {
-    const socket = new WebSocket('ws://cursorarena.vovaauer.com');
+    const socket = new WebSocket('wss://cursorarena.vovaauer.com');
     let localPlayerId = null;
 
     const canvas = mainContent.querySelector('canvas');
